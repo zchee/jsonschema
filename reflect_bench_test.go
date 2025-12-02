3e0da090e11084fc604cc2a2c3927fa2726ff9dd
Koichi Shiraishi <zchee.io@gmail.com>
2025-12-02 22:57:30 +0900

    Add reflection benchmarks for embedded/comment scenarios
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- reflect_bench_test.go
+++ reflect_bench_test.go
@@ -9,3 +9,45 @@ func BenchmarkReflectTestUser(b *testing.B) {
 		_ = r.Reflect(TestUser{})
 	}
 }
+
+func BenchmarkReflectEmbedded(b *testing.B) {
+	b.ReportAllocs()
+	r := &Reflector{}
+	for i := 0; i < b.N; i++ {
+		_ = r.Reflect(Outer{})
+	}
+}
+
+func BenchmarkReflectDoNotReference(b *testing.B) {
+	b.ReportAllocs()
+	r := &Reflector{DoNotReference: true}
+	for i := 0; i < b.N; i++ {
+		_ = r.Reflect(Outer{})
+	}
+}
+
+func BenchmarkReflectExpandedStruct(b *testing.B) {
+	b.ReportAllocs()
+	r := &Reflector{ExpandedStruct: true}
+	for i := 0; i < b.N; i++ {
+		_ = r.Reflect(Outer{})
+	}
+}
+
+func BenchmarkReflectWithComments(b *testing.B) {
+	b.ReportAllocs()
+	r := &Reflector{
+		CommentMap: map[string]string{
+			"github.com/invopop/jsonschema.Outer":           "outer comment",
+			"github.com/invopop/jsonschema.Outer.Text":      "text comment",
+			"github.com/invopop/jsonschema.Outer.Inner":     "inner comment",
+			"github.com/invopop/jsonschema.Inner.Foo":       "foo comment",
+			"github.com/invopop/jsonschema.Text":            "text type comment",
+			"github.com/invopop/jsonschema.TextNamed":       "text named comment",
+			"github.com/invopop/jsonschema.TextNamed.Field": "unused",
+		},
+	}
+	for i := 0; i < b.N; i++ {
+		_ = r.Reflect(Outer{})
+	}
+}
